567a3da2662532a1a4a870de141f47c20085ebef
sAz-G <sharifazem49@gmail.com>
2024-02-20 11:19:59 +0100

    update

--- plot_logged_pos.py
+++ plot_logged_pos.py
@@ -2,16 +2,37 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 #plt.rcParams['axes.grid'] = True
+plt.rcParams['axes.grid'] = False
 
+plt.rcParams.update({
+     "text.usetex": True,
+     "font.family": "Serif"
+})
+plt.rcParams.update({'font.size': 17})
 if __name__ == '__main__':
 
-    pths = [#'/home/saz/GitHub/quad-swarm-rl/logged_cfclient/20240121T14-17-39/Pose-20240121T14-19-12.csv',
-            #'/home/saz/GitHub/quad-swarm-rl/logged_cfclient/20240109T20-42-42/Pose-20240109T20-51-01.csv'
-            #'/home/saz/GitHub/quad-swarm-rl/logged_cfclient/20240121T14-28-59/Pose-20240121T14-31-09.csv'
-            #'/home/saz/GitHub/quad-swarm-rl/logged_cfclient/20240121T14-28-59/Pose-20240121T14-31-03.csv'
-            #'/home/saz/GitHub/quad-swarm-rl/logged_cfclient/20240121T14-28-59/Pose-20240121T14-30-47.csv'
-        #'/home/saz/GitHub/quad-swarm-rl/logged_cfclient/20240121T14-28-59/Pose-20240121T14-30-17.csv'
-        '/home/saz/GitHub/quad-swarm-rl/logged_cfclient/20240121T14-28-59/Pose-20240121T14-29-23.csv'
+    pths = [
+        #'/home/saz/Desktop/qsrl_test/logged_cfclient/20240121T14-17-39/Pose-20240121T14-18-45.csv'
+        #'/home/saz/Desktop/qsrl_test/logged_cfclient/20240121T15-08-29/Pose-20240121T15-11-42.csv'
+        #'/home/saz/Desktop/qsrl_test/logged_cfclient/20240121T14-17-39/Pose-20240121T14-19-12.csv'
+        #'/home/saz/Desktop/qsrl_test/logged_cfclient/20240109T20-42-42/Pose-20240109T20-50-17.csv'
+        #'/home/saz/Desktop/qsrl_test/logged_cfclient/20240121T14-17-39/Pose-20240121T14-18-45.csv'
+        #'/home/saz/Desktop/qsrl_test/logged_cfclient/20240121T15-08-29/Pose-20240121T15-11-42.csv'
+        #'/home/saz/Desktop/qsrl_test/logged_cfclient/20240121T14-28-59/Pose-20240121T14-30-17.csv'
+
+        #'/home/saz/Desktop/qsrl_test/logged_cfclient/20240109T20-42-42/Pose-20240109T20-50-17.csv'
+        #'/home/saz/Desktop/qsrl_test/logged_cfclient/20240121T14-17-39/Pose-20240121T14-18-19.csv'
+        #'/home/saz/Desktop/qsrl_test/logged_cfclient/20240121T14-17-39/Pose-20240121T14-18-45.csv'
+        #'/home/saz/Desktop/qsrl_test/logged_cfclient/20240121T14-17-39/Pose-20240121T14-19-12.csv'
+        #'/home/saz/Desktop/qsrl_test/logged_cfclient/20240121T14-28-59/Pose-20240121T14-29-23.csv'
+        #'/home/saz/Desktop/qsrl_test/logged_cfclient/20240121T14-28-59/Pose-20240121T14-29-44.csv'
+        #'/home/saz/Desktop/qsrl_test/logged_cfclient/20240121T14-28-59/Pose-20240121T14-30-17.csv'
+        #'/home/saz/Desktop/qsrl_test/logged_cfclient/20240121T14-28-59/Pose-20240121T14-30-47.csv'
+        #'/home/saz/Desktop/qsrl_test/logged_cfclient/20240121T14-28-59/Pose-20240121T14-31-03.csv'
+        #'/home/saz/Desktop/qsrl_test/logged_cfclient/20240121T15-08-29/Pose-20240121T15-10-47.csv'
+        #'/home/saz/Desktop/qsrl_test/logged_cfclient/20240121T15-08-29/Pose-20240121T15-11-42.csv'
+        #'/home/saz/Desktop/qsrl_test/logged_cfclient/20240109T20-42-42/Pose-20240109T20-51-01.csv'
+        #'/home/saz/Desktop/qsrl_test/logged_cfclient/20240109T20-42-42/Pose-20240109T20-51-01.csv'
             ]
 
     arr_pos = np.zeros((655, 3))
@@ -40,13 +61,25 @@ if __name__ == '__main__':
 
     x = np.linspace(0,1,1000)
 
-
     fig, ax = plt.subplots(1,1)
-    ax.plot(arr_pos[:,1],arr_pos[:,2])
-    #ax.plot(.5*np.cos(2*np.pi*x),.5*np.sin(2*np.pi*x), '--')
+    #ax.plot(arr_pos[:325,1],arr_pos[:325,0])
+    ax.plot(arr_pos[150:-350,1],arr_pos[150:-350,0])
+    #ax.plot(arr_pos[110:-90,1],arr_pos[80:-120,0])
+    #ax.plot([-0.85, -0.85], [1, 0.3], '--', color='0.45')
+    #ax.plot([-0.85, 1], [1, 1], '--', color='0.45')
+    #ax.plot([0.3, -0.6], [-0.8, -0.8], '--', color='0.45')
+    #ax.plot([-0.6, -0.6], [-0.8, 0.3], '--', color='0.45')
+    #ax.plot([0.3, -0.6], [0.3, 0.3], '--', color='0.45')
+
+
+    #ax.plot([arr_pos[0,1], -0.6], [arr_pos[0,0], -0.5], '--', color='0.45')
+    #ax.plot([arr_pos[0,1], -0.6], [arr_pos[0,0], -0.5], '--', color='0.45')
+    #ax.plot([arr_pos[0,1], -0.6], [arr_pos[0,0], -0.5], '--', color='0.45')
+    #ax.plot([-0.6, 0.5], [-0.5, -1], '--',  color='0.45')
+   # ax.plot(0.5*np.cos(2*np.pi*x),0.5*np.sin(2*np.pi*x), '--',  color='0.45')
     #ax.plot(arr_pos[:,1])
     #ax.plot(arr_pos[:,2])
-    ax.set_xlabel("y")
-    ax.set_ylabel("x")
+    ax.set_xlabel("$y$ (m)")
+    ax.set_ylabel("$x$ (m)")
 
     plt.show()
--- plot_logged_pos_3d.py
+++ plot_logged_pos_3d.py
@@ -1,6 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+plt.rcParams['axes.grid'] = False
+
+plt.rcParams.update({
+     "text.usetex": True,
+     "font.family": "Serif"
+})
+plt.rcParams.update({'font.size': 15})
 #plt.rcParams['axes.grid'] = True
 
 if __name__ == '__main__':
@@ -11,7 +18,8 @@ if __name__ == '__main__':
             #'/home/saz/GitHub/quad-swarm-rl/logged_cfclient/20240121T14-28-59/Pose-20240121T14-31-03.csv'
             #'/home/saz/GitHub/quad-swarm-rl/logged_cfclient/20240121T14-28-59/Pose-20240121T14-30-47.csv'
         #'/home/saz/GitHub/quad-swarm-rl/logged_cfclient/20240121T14-28-59/Pose-20240121T14-30-17.csv'
-        '/home/saz/GitHub/quad-swarm-rl/logged_cfclient/20240121T14-28-59/Pose-20240121T14-29-23.csv'
+        #'/home/saz/Desktop/qsrl_test/logged_cfclient/20240121T15-08-29/Pose-20240121T15-13-03.csv'
+        '/home/saz/Desktop/qsrl_test/logged_cfclient/20240121T15-08-29/Pose-20240121T15-12-17.csv'
             ]
 
     arr_pos = np.zeros((655, 3))
@@ -43,12 +51,15 @@ if __name__ == '__main__':
     fig = plt.figure()
     ax = plt.axes(projection='3d')
 
-    ax.plot(arr_pos[:,0], arr_pos[:,1],arr_pos[:,2])
-    ax.plot(np.cos(2*np.pi*x), np.sin(2*np.pi*x),x, '--')
+    ax.plot(arr_pos[:-400,0], arr_pos[:-400,1],0.8*arr_pos[:-400,2])
+    ax.plot(-0.6+0.8*np.cos(2*np.pi*x), 0.8*np.sin(2*np.pi*x),0.9*x, '--', color='0.4')
+    #ax.set_yticks([0.,0.2,0.4,0.6,0.8,1.0])
+    #ax.set_yticklabels(['0.','','0.2','','0.4','','0.6','','0.8','','1.0'])
+
     #ax.plot(arr_pos[:,1])
     #ax.plot(arr_pos[:,2])
-    ax.set_xlabel("x")
-    ax.set_ylabel("y")
-    ax.set_ylabel("z")
+    ax.set_xlabel("$x$ (m)")
+    ax.set_ylabel("$y$ (m)")
+    ax.set_zlabel("$z$ (m)")
 
     plt.show()
